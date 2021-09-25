d27449a1ac66b9372cd8c060494bdad29882b906
KateDen <denisova.ea@phystech.edu>
2021-09-25 12:19:52 +0300

    Update Onegin_sort.cpp

--- Onegin_sort.cpp
+++ Onegin_sort.cpp
@@ -5,55 +5,40 @@
 #include <locale.h>
 #include <TXLib.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/file.h>
 #include <assert.h>
 #include <errno.h>
 
-int FileReader (const unsigned int MAXLETTERS, char **Index, FILE* fileread);
-int Comparator1 (char **Index, unsigned MAXLINES);
-FILE *FileWriter (unsigned MAXLINES, char **Index);
+int FileBufferize (struct text *my_first_structure);
+int IndexFiller (int MAXLETTERS, char **Index, char *buffer);
+int Sort1 (char **Index, int MAXLINES);
+FILE *FileWriter (int MAXLINES, char **Index);
+
+struct text
+{
+    char *buffer;
+    int MAXLINES;
+    int MAXLETTERS;
+};
 
 int main ()
 {
     /**setlocale (LC_ALL, "Russian");
     qsort (void *getlines, MAXLINES, size_t MAXWORDS, strcmp (*line1, *line2)); **/
 
-    FILE *fileread = fopen ("Exampletext.txt", "rb");
-
-    if ((ferror (fileread)) != 0)
-        printf ("File reading error!");
+    struct text my_first_structure = {};
 
 
-    fseek (fileread, 0, SEEK_END);
+    FileBufferize (&my_first_structure);
 
-    long cur_pos = ftell(fileread);
+    char *Index [my_first_structure.MAXLINES];
 
-    fseek (fileread, 0, SEEK_SET);
+    IndexFiller (my_first_structure.MAXLETTERS, Index, my_first_structure.buffer);
 
-    const unsigned int MAXLETTERS = cur_pos;
-    unsigned MAXLINES = 0;
-    int symbol = 0;
+    Sort1 (Index, my_first_structure.MAXLINES);
 
+    FileWriter (my_first_structure.MAXLINES, Index);
 
-    while ((symbol = fgetc (fileread)) != EOF)
-    {
-        if (symbol == '\n')
-            MAXLINES++;
-    }
-
-    fseek (fileread, 0, SEEK_SET);
-
-    cur_pos = ftell(fileread);
-
-
-    char *Index [MAXLINES];
-
-    FileReader (MAXLETTERS, Index, fileread);
-    Comparator1 (Index, MAXLINES);
-    FileWriter (MAXLINES, Index);
-
-    fclose (fileread);
+    //qsort ();
 
 
     return 0;
@@ -62,9 +47,64 @@ int main ()
 
 //-----------------------------------------------------------------------------
 
-
-int FileReader (const unsigned int MAXLETTERS, char **Index, FILE *fileread)
+int FileBufferize (struct text *my_first_structure)
 {
+    int size_of_element = sizeof (char);
+
+    FILE *fileread = fopen ("Exampletext.txt", "rb");
+
+    if ((ferror (fileread)) != 0)
+    {
+        printf ("File reading error!\n");
+        return 0;
+    }
+
+
+    fseek (fileread, 0, SEEK_END);
+    my_first_structure -> MAXLETTERS = ftell(fileread);
+    fseek (fileread, 0, SEEK_SET);
+
+
+    my_first_structure -> buffer = (char *) calloc (my_first_structure -> MAXLETTERS + 1, size_of_element);
+
+    if  ((fread (my_first_structure-> buffer, size_of_element, my_first_structure -> MAXLETTERS, fileread))
+    != (my_first_structure -> MAXLETTERS + 1))
+    {
+        printf ("Fread error!\n");
+        return 0;
+    }
+
+
+    int counter = 0;
+    int symbol = 0;
+
+    while ((symbol = getc (my_first_structure -> buffer)) != '\0')
+    {
+        if (symbol == '\n')
+        {
+            symbol = '\0';
+            counter++;
+        }
+    }
+
+    my_first_structure -> MAXLINES = counter + 1;
+
+    if ((fseek (fileread, 0, SEEK_SET)) != 0)
+        printf ("File seek error!\n");
+
+
+    if ((fclose (fileread)) == EOF)
+        printf ("File close error!\n");
+
+    return 0;
+}
+
+
+int IndexFiller (int MAXLETTERS, char **Index, FILE *fileread)
+{
+    assert (Index);
+
+
     char str [MAXLETTERS + 1];
     int i = 0;
 
@@ -72,11 +112,9 @@ int FileReader (const unsigned int MAXLETTERS, char **Index, FILE *fileread)
     while (fgets (str, MAXLETTERS + 1, fileread) != NULL)
     {
         Index [i] = strdup(str);
-
         i++;
         strcpy (str, "Exampletext.txt");
     }
-
 
     /*int num_lines = i;
     //printf ("%c\n", str [45]);
@@ -94,7 +132,7 @@ int FileReader (const unsigned int MAXLETTERS, char **Index, FILE *fileread)
 //-----------------------------------------------------------------------------
 
 
-int Comparator1 (char **Index, unsigned MAXLINES)
+int Sort1 (char **Index, int MAXLINES)
 {
     //int length = sizeof (*Index) / sizeof (int);
 
@@ -105,9 +143,9 @@ int Comparator1 (char **Index, unsigned MAXLINES)
     {
         counter = 0, maxrow = 0;
 
-        for (unsigned k = 0; k <= MAXLINES - 1; k++)
+        for (int k = 0; k <= MAXLINES - 1; ++k)
         {
-            for (i = 0; i < (MAXLINES - k - 1); i++)
+            for (i = 0; i < (MAXLINES - k - 1); ++i)
             {
                 int str_comp_res = strcmp (Index [i], Index [i+1]);
 
@@ -118,25 +156,27 @@ int Comparator1 (char **Index, unsigned MAXLINES)
                     Index [i+1] = tmp;
 
                     counter = 1;
-                    maxrow++;
+                    ++maxrow;
                 }
             }
         }
     }
 
-    for (i = 0; i < MAXLINES; i++)
-        puts (Index [i]);
+    //for (i = 0; i < MAXLINES; i++)
+        //puts (Index [i]);
 
 
 
     return 0;
 }
 
-FILE *FileWriter (unsigned MAXLINES, char **Index)
+FILE *FileWriter (int MAXLINES, char **Index)
 {
-    FILE *filewrite = fopen ("Hamlet_sorted.txt", "a");
+    FILE *filewrite = fopen ("Hamlet_sort.txt", "wb");
+    assert (filewrite != NULL);
 
-    for (unsigned i = 0; i < MAXLINES; i++)
+
+    for (int i = 0; i < MAXLINES; i++)
     {
         fputs (Index [i], filewrite);
     }

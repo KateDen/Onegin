05b404b0a38a9856712bf1c985cd105f2dee68bd
KateDen <denisova.ea@phystech.edu>
2021-10-05 22:16:39 +0300

    Update Str_func.h

--- Str_func.h
+++ Str_func.h
@@ -8,6 +8,36 @@
 #include <assert.h>
 #include <errno.h>
 
+
+enum error_consts {
+    FOPEN_ERR    = 1,
+    EOF_READ_ERR = 2,
+    FREAD_ERR    = 3,
+    FSEEK_ERR    = 4,
+    FCLOSE_ERR   = 5,
+    SWAP_ERR     = 6
+};
+
+
+//typedef         //?
+struct Text
+{
+    char *buffer;
+    int file_size;
+
+    int num_lines;
+    struct Line* lines;
+};
+
+//typedef            //?
+struct Line
+{
+    char *str;
+    int length;
+};
+
+
+
 int Text_Ctor (FILE *fileread, struct Text *text);
 int Text_Dtor(struct Text *text);
 
@@ -19,7 +49,7 @@ int Sort1 (struct Text *text, int numlines);
 
 int JustSwap (struct Text *text, int i);
 
-int FileWriter (struct Text *text);
+int FileWriter (struct Text *text, FILE *filewrite);
 
-void ErrorPrints (void);
+int ErrorPrints (void);
 

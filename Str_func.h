3005e82dab1edb7dcc6f5e074956a6e47cbe7b08
KateDen <denisova.ea@phystech.edu>
2021-10-07 06:13:04 +0300

    Update Str_func.h

--- Str_func.h
+++ Str_func.h
@@ -3,11 +3,9 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <locale.h>
-#include <TXLib.h>
 #include <sys/types.h>
 #include <assert.h>
 #include <errno.h>
-
 
 enum error_consts {
     FOPEN_ERR    = 1,
@@ -19,36 +17,40 @@ enum error_consts {
 };
 
 
-//typedef         //?
+// size_t - done
 struct Text
 {
-    char *buffer;
-    int file_size;
+    char *buffer = nullptr;
+    size_t file_size;
 
-    int num_lines;
-    struct Line* lines;
+    size_t num_lines;
+    struct Line *lines = nullptr;
 };
 
-//typedef            //?
 struct Line
 {
-    char *str;
-    int length;
+    char  *str;
+    size_t length;
 };
 
+//==================================================================================
 
+// см что принимают функции!
+// см сто возвращают
+// названия следить
 
-int Text_Ctor (FILE *fileread, struct Text *text);
-int Text_Dtor(struct Text *text);
+int text_Ctor (FILE *fileread, struct Text *text);
+int text_Dtor(struct Text *text);
 
-int FileReader (struct Text *text, FILE *fileread);
-int Bufferizer (struct Text *text, FILE *fileread);
-int InitStrings (struct Text *text);
+ssize_t file_sizer (struct Text *text, FILE *fileread);
+int create_buffer (struct Text *text, FILE *fileread);
+size_t str_counter (struct Text *text);
+int init_strings (struct Text *text);
 
 int Sort1 (struct Text *text, int numlines);
-
-int JustSwap (struct Text *text, int i);
+int just_swap (struct Text *text, int i);
 
 int FileWriter (struct Text *text, FILE *filewrite);
 
-int ErrorPrints (void);
+int error_prints (void);
+

98ad3816642f37149ea67eb9db0f719a07effd9f
KateDen <denisova.ea@phystech.edu>
2021-10-09 12:42:31 +0300

    Update Str_func.h
    almost finished funcs prots without sort

--- Str_func.h
+++ Str_func.h
@@ -9,15 +9,20 @@
 
 enum error_consts {
     FOPEN_ERR    = 1,
-    EOF_READ_ERR = 2,
+    PTR_ERR      = 2,
     FREAD_ERR    = 3,
     FSEEK_ERR    = 4,
     FCLOSE_ERR   = 5,
-    SWAP_ERR     = 6
+    SWAP_ERR     = 6,
+    FTELL_ERR    = 7,
+    FPUT_ERR     = 8
 };
 
+#define PRINT_LINE printf("I'm at line %d in %s\n", __LINE__, __PRETTY_FUNCTION__)
 
-// size_t - done
+#define PRINT_PTR(ptr) printf("I'm %s at %p\n", #ptr, ptr)
+
+
 struct Text
 {
     char *buffer = nullptr;
@@ -35,22 +40,19 @@ struct Line
 
 //==================================================================================
 
-// см что принимают функции!
-// см сто возвращают
-// названия следить
-
 int text_Ctor (FILE *fileread, struct Text *text);
-int text_Dtor(struct Text *text);
+void text_Dtor(struct Text *text);
 
-ssize_t file_sizer (struct Text *text, FILE *fileread);
-int create_buffer (struct Text *text, FILE *fileread);
+ssize_t file_sizer (FILE *fileread);
+char* create_buffer (struct Text *text, FILE *fileread);
 size_t str_counter (struct Text *text);
-int init_strings (struct Text *text);
+void init_strings (struct Text *text);
 
 int Sort1 (struct Text *text, int numlines);
 int just_swap (struct Text *text, int i);
 
-int FileWriter (struct Text *text, FILE *filewrite);
+void file_output (struct Text *text, FILE *filewrite);
+void file_original_output (struct Text *text, FILE *filewrite);
 
-int error_prints (void);
+int error_prints (int error);
 

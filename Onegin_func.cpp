46be3184842f0671cd78eec9f5f34dbe86ce5f04
KateDen <denisova.ea@phystech.edu>
2021-10-09 12:37:20 +0300

    Update Onegin_func.cpp
    almost finished funcs without sort

--- Onegin_func.cpp
+++ Onegin_func.cpp
@@ -5,25 +5,31 @@
 //check all returns!
 // nullptr
 // print original text - find the place with this note, paste there
+// assert -> if in create buffer
 
 
+extern int error;
 
 //-----------------------------------------------------------------------------
 
 
 int text_Ctor(FILE *fileread, struct Text *text) {
 
+    PRINT_LINE;
+    PRINT_PTR(fileread);
     assert (fileread != nullptr);
 
     // rename - done
-    text->file_size = file_sizer (fileread);
+    text -> file_size = file_sizer (fileread);
 
-    text->buffer = create_buffer (text, fileread);
+    text -> buffer = create_buffer (text, fileread);
     
-    text->num_lines = str_counter (text); //что принимает?
+    text -> num_lines = str_counter (text);  
 
-    text->lines = (struct Line*) calloc (text->num_lines, sizeof (Line));
+    text -> lines = (struct Line*) calloc (text -> num_lines, sizeof (Line));
     
+    PRINT_LINE;
+    PRINT_PTR(text->lines);
     assert (text->lines);
 
     init_strings (text);
@@ -31,13 +37,16 @@ int text_Ctor(FILE *fileread, struct Text *text) {
     return 0;
 }
 
-int text_Dtor(struct Text *text) {
+void text_Dtor(struct Text *text) {
+
+    PRINT_LINE;
+    PRINT_PTR(text);
+    assert (text != nullptr);
 
     free(text->lines);
     free(text->buffer);
     text->lines = nullptr;
     text->buffer = nullptr;
-    return 0;
 }
 
 
@@ -46,20 +55,23 @@ int text_Dtor(struct Text *text) {
 // rename - done
 ssize_t file_sizer (FILE *fileread) {
 
+    PRINT_LINE;
+    PRINT_PTR(fileread);
     assert (fileread != nullptr);
 
+
     if ((fseek (fileread, 0, SEEK_END)) == -1) {
-        return FSEEK_ERR;
+        return error_prints (FSEEK_ERR);
     }
 
     ssize_t file_size = ftell (fileread);
     
     if (file_size == -1) {
-        return FTELL_ERR;
+        return error_prints (FTELL_ERR);
     }
 
     if ((fseek (fileread, 0, SEEK_SET)) == -1) {
-        return FSEEK_ERR;
+        return error_prints (FSEEK_ERR);
     }
 
     return file_size;
@@ -68,29 +80,38 @@ ssize_t file_sizer (FILE *fileread) {
 
 //-----------------------------------------------------------------------------
 
-
+//хранить переменную кода ошибки в ctore и передавать ее в функции
 char* create_buffer (struct Text *text, FILE *fileread) {
 
-    assert(fileread != nullptr);        
+    PRINT_LINE;
+    PRINT_PTR(fileread);
+    assert(fileread != nullptr);
+    PRINT_LINE;
+    PRINT_PTR(text);
+    assert (text != nullptr);
+        
 
     text -> buffer = (char *) calloc ((text -> file_size + 1), sizeof (char));
+    assert (text -> buffer);                                                         //if!!
 
     int readsymb = fread (text -> buffer, sizeof (char), text -> file_size, fileread);
 
-    //в ассерте вызвать fread?
-    assert (readsymb != 0);
-    /*if (readsymb != (text -> file_size)) {   //ф-я не возвращает int
-        return FREAD_ERR;
-    }*/
+    //в ассерте вызвать fread? not good
+    PRINT_LINE;
+    assert (readsymb);
 
     text -> buffer[text -> file_size] = '\0';
     
     return text -> buffer;
 }
 
+//==================================================================================================
+
 // new func - done 
 size_t str_counter (struct Text *text) {
 
+    PRINT_LINE;
+    PRINT_PTR(text);
     assert (text != nullptr);
 
     char* begin_str = text -> buffer;         
@@ -102,9 +123,8 @@ size_t str_counter (struct Text *text) {
         ++numb_str;
         begin_str = end_str + 1;
     }
-
-    // переделать - done
-    if (*begin_str != '\n') {
+  
+    if (*begin_str != '\0') {
 
         ++numb_str;
     }
@@ -116,39 +136,50 @@ size_t str_counter (struct Text *text) {
 //-----------------------------------------------------------------------------
 
 
-int init_strings (struct Text *text) {
+void init_strings (struct Text *text) {
 
+    PRINT_LINE;
+    PRINT_PTR(text);
     assert (text != nullptr);
 
     size_t i = 0;
 
-    // rename - done
     char *begin_str = text->buffer;
-    char *end_str = text->buffer;
+    char *end_str   = nullptr;
 
-    // почти копипаст функции подсчёта строк - done
+    // if (*text->buffer == '\n')
 
-    while((end_str = strchr(begin_str, '\0')) != nullptr) {
+    while((end_str = strchr(begin_str, '\n')) != nullptr) {
 
-        text -> lines[i].str = begin_str;
+        text -> lines[i].str    = begin_str;
         text -> lines[i].length = end_str - begin_str;
+        
+        // проверка на end_str > text->buffer
+        if (*(end_str - 1) == '\r')
+        {
+            *(end_str - 1) = '\0';
+        }
+        else 
+        {
+            *end_str = '\0';
+        }
+
         begin_str = end_str + 1;
         ++i;
     }
 
-    // переделать - done
-    if (*begin_str != '\n') {
+    if (*begin_str != '\0') {
 
-        *(text -> lines[i].str) = '\n';
+        text -> lines[i].str = begin_str;
+        text -> lines[i].length = text->buffer + text->file_size - begin_str;
     }
 
     for (i = 0; i < text->num_lines; i++) {
 
-        fputs (text->lines[i].str, stdout);
+        // EOF == ?
+        fputs (text->lines[i].str, stdout);       //проверки тут?
         fputc ('\n', stdout);
     }
-
-    return 0;
 }
 
 //-----------------------------------------------------------------------------
@@ -158,33 +189,41 @@ int init_strings (struct Text *text) {
 // print original text - 1
 // strchr() fputs - much faster than putc
 
-int file_output (struct Text *text, FILE *filewrite) {     
+void file_output (struct Text *text, FILE *filewrite) {     
 
-    assert (filewrite != 0);
-    // проверки - done
+    assert (text != nullptr);
+    assert (filewrite != nullptr);
+
     for (size_t i = 0; i < text->num_lines; i++) {
 
-        fputs (text->lines[i].str, filewrite);          //проверки тут?
+        fputs(text->lines[i].str, filewrite);          //проверки тут?
         fputc('\n', filewrite);
     }
-
-    return 0;
 }
 
-int file_original_output (struct Text *text, FILE *filewrite) {
 
+//===================================================================================
+
+
+void file_original_output (struct Text *text, FILE *filewrite) {
+
+    assert (text != nullptr);
     assert (filewrite != nullptr);
 
     char *str = text -> buffer;
 
-    for (size_t i = 0; i <= text -> num_lines; ) {
+    for (size_t i = 0; i < text -> num_lines; i++) {
 
-        fputs (str, filewrite);
+        fputs (str, filewrite);                            //проверки тут?
         fputc ('\n', filewrite);
+        // + 2
         str = strchr (str, '\0') + 1;
- 
+
+        if (*str == '\n')
+        {
+            ++str;
+        }
     }
-    return 0;
 }
 
 
